32ad9ef15219082f482f685738306da15308ee38
hebbs <hebbsnator@gmail.com>
2024-09-13 19:55:48 +0300

    sixth exercise 0

--- sixth.py
+++ sixth.py
@@ -1,4 +1,3 @@
-  next  |  chance
 
 # Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
 
@@ -11,3 +10,14 @@ def combo_string(a, b):
   else:
     return b + a +b
 
+# Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+# non_start('Hello', 'There') → 'ellohere'
+# non_start('java', 'code') → 'avaode'
+# non_start('shotl', 'java') → 'hotlava'
+
+def non_start(a, b):
+  if len(a) + len(b)  == 2:
+    return ""
+  else: 
+    return a[1:] + b[1:]
