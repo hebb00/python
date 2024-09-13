269e2dfcafb32b10d101a82f10e15cd87b2f932a
hebbs <hebbsnator@gmail.com>
2024-09-13 20:31:31 +0300

    sixth exercise 000

--- sixth.py
+++ sixth.py
@@ -33,3 +33,17 @@ def left2(str):
     return str
   else: 
     return str[2:] + str[:2]
+
+# When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+
+# cigar_party(30, False) → False
+# cigar_party(50, False) → True
+
+def cigar_party(cigars, is_weekend):
+  return (is_weekend and cigars >= 40) or (not is_weekend and  cigars >= 40 and  cigars <= 60)
+
+def cigar_party(cigars, is_weekend):
+  if is_weekend:
+    return (cigars >= 40)
+  else:
+    return (cigars >= 40 and cigars <= 60)
