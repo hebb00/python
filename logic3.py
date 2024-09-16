6c07b61dc35bc022833162a9701ee335b6a79970
hebbs <hebbsnator@gmail.com>
2024-09-16 14:53:47 +0300

    logic exe 0

--- logic3.py
+++ logic3.py
@@ -22,4 +22,22 @@ def caught_speeding(speed, is_birthday):
 def same_first_last(nums):
   return len(nums) >= 1 and nums[0] == nums[len(nums)-1]
 
-    
+
+
+# Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+
+# alarm_clock(1, False) → '7:00'
+# alarm_clock(5, False) → '7:00'
+# alarm_clock(0, False) → '10:00'
+
+def alarm_clock(day, vacation):
+  clock = "7:00"
+  clock0 = "10:00"
+  if vacation:
+    clock = "10:00"
+    clock0 = "off"
+  if day in range(1,6):
+    return clock
+  else: 
+    return clock0
+  
