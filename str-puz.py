14d1fca6b76ac025c75d94fef6cdc72c8fb9892e
hebbs <hebbsnator@gmail.com>
2024-09-20 18:26:30 +0300

    another solustion for string puzzle

--- str-puz.py
+++ str-puz.py
@@ -12,11 +12,17 @@ def count_code(str):
         if str[i+3] =='e':
           count+=1
              
-  
-
         
   return count
   
 
         
+def count_code(str):
+  count = 0
+
+  for i in range(len(str)-1):
+    if str[i] + str[i+1 ] == 'co' and i + 3 <  len(str) and str[i+3] =='e':
+          count+=1
+             
+  
   return count
