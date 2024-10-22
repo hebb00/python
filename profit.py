aa0ff1584245b9d9067736e3b2dd890d5b525a98
hebbs <hebbsnator@gmail.com>
2024-10-22 21:02:53 +0300

    maximum profit

---
+++ profit.py
@@ -0,0 +1,37 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """  
+    dif = 0
+    for i in range(len(prices)):
+        for j in range(i+1,len(prices)):
+            if dif <  prices[j] - prices[i]:
+                dif = prices[j] - prices[i] 
+    return dif
+
+
+if __name__ == "__main__":
+    
+        test = [5,4,3]
+        print(maxProfit(test))
+        
+    
+       
+        
+    
